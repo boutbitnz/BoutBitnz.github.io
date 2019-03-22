cafbf6519aab1edceedffd095def7767827b1bf0
boutbitnz <boutbitnz2@gmail.comm>
2019-03-22 02:23:42 -0500

    feature/core_services: Configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppShellComponent } from './components/app-shell/app-shell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppShellComponent
   ],
   imports: [
     BrowserModule,
