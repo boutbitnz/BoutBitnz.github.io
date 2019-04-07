0d76874ff5d2c865d8c83fba2489a304f0369960
boutbitnz <boutbitnz2@gmail.comm>
2019-04-07 01:15:01 -0500

    feature/component_entry: Disabled error implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -97,7 +97,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    // throw new Error('Method not implemented.');
   }
 
 }
