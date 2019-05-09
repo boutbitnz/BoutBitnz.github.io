d9068b889153d06b35be4e3180d0e96fc1883eff
boutbitnz <boutbitnz2@gmail.comm>
2019-05-09 03:24:28 -0500

    feature/animation_toggle_fix: Universal file addition

--- src/main.ts
+++ src/main.ts
@@ -12,19 +12,36 @@
 //   .catch(err => console.error(err));
 
 
-import './polyfills';
+// import './polyfills';
 
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { enableProdMode } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+// import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+// platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
+//   // Ensure Angular destroys itself on hot reloads.
+//   if (window['ngRef']) {
+//     window['ngRef'].destroy();
+//   }
+//   window['ngRef'] = ref;
 
-  // Otherise, log the boot error
-}).catch(err => console.error(err));
+//   // Otherise, log the boot error
+// }).catch(err => console.error(err));
+
+
+
+// Universal starter https://github.com/angular/universal-starter/blob/master/src/main.ts
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  platformBrowserDynamic().bootstrapModule(AppModule);
+});
